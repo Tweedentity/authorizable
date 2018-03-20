89cb65894e536c6fdbdedde592592bded5bffd61
sullof <sullof@gmail.com>
2018-03-20 13:02:39 -0700

    Removing unnecessary dependencies

--- truffle.js
+++ truffle.js
@@ -1,19 +1,9 @@
-const HDWalletProvider = require("truffle-hdwallet-provider")
-const mnemonic = process.env.MNEMONIC
-const infuraUrl = process.env.INFURA_URL
-
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*"
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, infuraUrl)
-      },
-      network_id: 3
     }
   },
   solc: {
